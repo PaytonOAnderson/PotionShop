552daedb99955bbc67d02ee20545fa22bd649100
Payton Anderson <poanders@calpoly.edu>
2024-04-14 15:22:37 -0700

    red blue barrels

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -4,6 +4,7 @@ from src.api import auth
 import sqlalchemy
 from src import database as db
 from src.api.inventory import INVENTORY
+import random
 
 router = APIRouter(
     prefix="/barrels",
@@ -29,6 +30,12 @@ def post_deliver_barrels(barrels_delivered: list[Barrel], order_id: int):
             if barrels.potion_type == [0, 1, 0, 0]:
                 connection.execute(sqlalchemy.text(f"Update {INVENTORY} SET gold = gold - {barrels.price}"))
                 connection.execute(sqlalchemy.text(f"Update {INVENTORY} SET num_green_ml = num_green_ml + {barrels.ml_per_barrel}"))
+            if barrels.potion_type == [1, 0, 0, 0]:
+                connection.execute(sqlalchemy.text(f"Update {INVENTORY} SET gold = gold - {barrels.price}"))
+                connection.execute(sqlalchemy.text(f"Update {INVENTORY} SET num_red_ml = num_red_ml + {barrels.ml_per_barrel}"))
+            if barrels.potion_type == [0, 0, 1, 0]:
+                connection.execute(sqlalchemy.text(f"Update {INVENTORY} SET gold = gold - {barrels.price}"))
+                connection.execute(sqlalchemy.text(f"Update {INVENTORY} SET num_blue_ml = num_blue_ml + {barrels.ml_per_barrel}"))
 
     return "OK"
 
@@ -46,16 +53,31 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
         green_potions_table = connection.execute(sqlalchemy.text(f"SELECT num_green_potions FROM {INVENTORY}"))
         for row in green_potions_table:
             green_potions = row[0]
-        # currently buying one barrel if less than ten potions and can afford it
-        if green_potions < 10:
-            for barrel in wholesale_catalog:
-                if barrel.sku == "SMALL_GREEN_BARREL" and gold >= barrel.price:
-                    return [
-                        {
-                            "sku": "SMALL_GREEN_BARREL",
-                            "quantity": 1
-                        }
-                    ]
+        # currently buying one small barrel of a random color if I can afford it
+        
+        random_num = random.randint(1, 3)
+        for barrel in wholesale_catalog:
+            if random_num == 1 and barrel.sku == "SMALL_GREEN_BARREL" and gold >= barrel.price:
+                return [
+                    {
+                        "sku": "SMALL_GREEN_BARREL",
+                        "quantity": 1
+                    }
+                ]
+            if random_num == 2 and barrel.sku == "SMALL_RED_BARREL" and gold >= barrel.price:
+                return [
+                    {
+                        "sku": "SMALL_RED_BARREL",
+                        "quantity": 1
+                    }
+                ]
+            if random_num == 3 and barrel.sku == "SMALL_BLUE_BARREL" and gold >= barrel.price:
+                return [
+                    {
+                        "sku": "SMALL_BLUE_BARREL",
+                        "quantity": 1
+                    }
+                ]
     return []
         
 
--- src/api/catalog.py
+++ src/api/catalog.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 import sqlalchemy
 from src import database as db
+from src.api.inventory import INVENTORY
 
 router = APIRouter()
 
@@ -10,13 +11,43 @@ def get_catalog():
     """
     Each unique item combination must have only a single price.
     """
-
-    return [
-            {
-                "sku": "GREEN_POTION_0",
-                "name": "green potion",
-                "quantity": 1,
-                "price": 25,
-                "potion_type": [0, 100, 0, 0],
-            }
-        ]
+    with db.engine.begin() as connection:
+        green_potions_table = connection.execute(sqlalchemy.text(f"SELECT num_green_potions FROM {INVENTORY}"))
+        for row in green_potions_table:
+            green_potions = row[0]
+        red_potions_table = connection.execute(sqlalchemy.text(f"SELECT num_green_potions FROM {INVENTORY}"))
+        for row in red_potions_table:
+            red_potions = row[0]
+        blue_potions_table = connection.execute(sqlalchemy.text(f"SELECT num_green_potions FROM {INVENTORY}"))
+        for row in blue_potions_table:
+            blue_potions = row[0]
+        if green_potions > 0:
+            return [
+                    {
+                        "sku": "GREEN_POTION_0",
+                        "name": "green potion",
+                        "quantity": 1,
+                        "price": 25,
+                        "potion_type": [0, 100, 0, 0],
+                    }
+                ]
+        elif red_potions > 0:
+            return [
+                    {
+                        "sku": "RED_POTION_0",
+                        "name": "green potion",
+                        "quantity": 1,
+                        "price": 25,
+                        "potion_type": [100, 0, 0, 0],
+                    }
+                ]
+        elif blue_potions > 0:
+            return [
+                    {
+                        "sku": "BLUE_POTION_0",
+                        "name": "green potion",
+                        "quantity": 1,
+                        "price": 30,
+                        "potion_type": [100, 0, 0, 0],
+                    }
+                ]
