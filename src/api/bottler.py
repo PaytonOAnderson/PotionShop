d9d2b1dc91d9eb28b55823d4ecbd68a92a6f349b
Payton Anderson <poanders@calpoly.edu>
2024-04-29 21:45:41 -0700

    fixed database update

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -25,9 +25,11 @@ def post_deliver_bottles(potions_delivered: list[PotionInventory], order_id: int
     with db.engine.begin() as connection:
         for potion in potions_delivered:
             # update item table to include potion created
-            connection.execute(sqlalchemy.text("UPDATE testing_global_inventory SET num_red_ml = num_red_ml - :red_qty * :qty, num_green_ml = num_green_ml - :green_qty * :qty, num_blue_ml = num_blue_ml - :blue_qty * :qty, num_dark_ml = num_dark_ml - :dark_qty * :qty, num_potions = num_potions + :qty"),
+            connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_ml = num_red_ml - :red_qty * :qty, num_green_ml = num_green_ml - :green_qty * :qty, num_blue_ml = num_blue_ml - :blue_qty * :qty, num_dark_ml = num_dark_ml - :dark_qty * :qty, num_potions = num_potions + :qty"),
                                [{"red_qty" :potion.potion_type[0], "green_qty" : potion.potion_type[1], "blue_qty" : potion.potion_type[2], "dark_qty" : potion.potion_type[3], "qty" : potion.quantity}])
             connection.execute(sqlalchemy.text("UPDATE testing_items SET qty = qty + :qty WHERE red_qty = :red_qty AND green_qty = :green_qty AND blue_qty = :blue_qty AND dark_qty = :dark_qty") ,[{"qty" : potion.quantity, "red_qty" : potion.potion_type[0], "green_qty" : potion.potion_type[1], "blue_qty" : potion.potion_type[2], "dark_qty" : potion.potion_type[3]}])
+            transaction_id = connection.execute(sqlalchemy.text("INSERT INTO account_transactions (description) VALUES ('Me recieving :qty potionsgreen barrels with :ml mls') RETURNING id"), [{"price" : barrels.price, "qty" : barrels.quantity, "ml" : barrels.ml_per_barrel}]).first()[0]
+
     return "OK"
 
 @router.post("/plan")
